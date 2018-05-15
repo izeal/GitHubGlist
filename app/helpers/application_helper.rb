0043897a619208f5ac75ba154c5d611d13744616
izeal <ivanz_89@mail.ru>
2018-05-15 19:14:15 +0300

    add bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def errors_viewer(resource)
+    return nil if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div id="error_explanation">
+      <p>#{sentence}</p>
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def full_title(title = '')
+    base_title = 'GitHubGlist'
+    title == '' ? base_title : "#{title} | #{base_title}"
+  end
 end
